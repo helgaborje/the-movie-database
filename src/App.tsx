568d2355ceac3cc610c800118a585448794dccac
Helga Börjesson <helgaborjesson@gmail.com>
2023-08-23 11:13:55 +0200

    added a homepage and app.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import './assets/scss/App.scss'
 
 const App = () => {
 	return (
 		<>
-			<ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+			<Routes>
+				<Route path='/' element={<HomePage/>} />
+			</Routes>
+			{/* <ReactQueryDevtools initialIsOpen={false} position="bottom-right" /> */}
 		</>
 	)
 }
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import Container from "react-bootstrap/Container"
+
+const HomePage = () => {
+
+	return (
+		<>
+			<Container>
+				<h1>Welcome to TMDB</h1>
+			</Container>
+		</>
+	)
+}
+
+export default HomePage
