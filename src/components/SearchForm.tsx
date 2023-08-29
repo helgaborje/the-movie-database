2c00d6b58713fb0c5632cfbe1352fc2290e9d221
Helga Börjesson <helgaborjesson@gmail.com>
2023-08-29 11:53:56 +0200

    made a searchForm component

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+
+interface SearchFormProps  {
+    value: string
+    onChange: (e: any) => void
+    onSubmit: (e: React.FormEvent) => void
+}
+
+const SearchForm: React.FC<SearchFormProps> = ({ value, onChange, onSubmit }) => {
+	return (
+		<>
+			<Form className='mb-4' onSubmit={onSubmit}>
+				<Row>
+					<Col xs="auto">
+						<Form.Control
+							onChange={onChange}
+							value={value}
+							type="text"
+							placeholder="Search for movie"
+							className=" mr-sm-2"
+						/>
+					</Col>
+					<Col xs="auto">
+						<Button type="submit">Submit</Button>
+					</Col>
+				</Row>
+			</Form>
+		</>
+	)
+}
+
+export default SearchForm
